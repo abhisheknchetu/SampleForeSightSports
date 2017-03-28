dec87b482cbe2fcd2c8cfdb3f5b3e2b43fa6e97d
Abhishek Nigam <abhishekn@chetu.com>
2017-03-28 17:41:06 +0530

    Initial commit

---
+++ app/src/main/java/foresightsports_fs/com/sampleforesightsports/BaseActivity.java
@@ -0,0 +1,18 @@
+package foresightsports_fs.com.sampleforesightsports;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by abhishekn on 3/27/2017.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
